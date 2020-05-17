d03adafc02e1ba05875633c398d41cb7a5542de0
Dumon <dumon.dmd@gmail.com>
2020-05-17 17:30:03 -0300

    Utilizando o belongsTo

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,41 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Produto;
+use App\Categoria;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categorias', function () {
+   $cats = Categoria::all();
+   if(count($cats) === 0)
+   		echo "<h4>Você não possui nenhuma categoria cadastrada</h4>";
+   	else {
+   		foreach ($cats as $c) {
+   			echo "<p>" . $c->id . " - " . $c->nome . "</p>";
+   		}
+   	}
+});
+
+
+Route::get('/produtos', function () {
+   $prods = Produto::all();
+   if(count($prods) === 0)
+   		echo "<h4>Você não possui nenhuma produto cadastrado</h4>";
+   	else {
+   		echo "<table>";
+   		echo "<thead><tr><td>Nome</td><td>Estoque</td><td>Preço</td><td>Categoria</td></tr></thead>";
+   		foreach ($prods as $p) {
+   			echo "<tr>";
+   			echo "<td>" . $p->nome . "</td>";
+   			echo "<td>" . $p->estoque . "</td>";
+   			echo "<td>" . $p->preco . "</td>";
+   			echo "<td>" . $p->categoria->nome . "</td>";
+   			echo "</tr>";
+   		}
+   		echo "</table>";
+   	}
+});
+
